ba8017112cfb30ef5ba810eb27a9b07034eac735
CoDeX-Ray <raymundo3deborja@gmail.com>
2024-11-16 13:06:34 +0800

    Java Exercise #3

---
+++ src/jvEx3.java
@@ -0,0 +1,12 @@
+/*
+ Problem 3: Write a Java program to divide two numbers and print them on the screen.
+ Test Data : 50/3
+ Expected Output : 16
+*/
+
+public class jvEx3 {
+    public static void main(String[] args) {
+        int numberOne = 50, numberTwo = 3, quotient = numberOne / numberTwo;
+        System.out.println(quotient);
+    }
+}
