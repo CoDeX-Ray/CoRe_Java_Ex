aa6552e4762ae05247e3fbc29584ba593d809bc2
CoDeX-Ray <raymundo3deborja@gmail.com>
2024-11-16 12:32:44 +0800

    Java Exercise 2

---
+++ src/jvEx2.java
@@ -0,0 +1,12 @@
+/*
+ Problem 2: Write a Java program to print the sum of two numbers.
+ Test Data: 74 + 36
+ Expected Output : 110
+*/
+
+public class jvEx2 {
+    public static void main(String[] args) {
+        int numberOne = 74, numberTwo = 36, sum = numberOne + numberTwo;
+        System.out.println(sum);
+    }
+}
