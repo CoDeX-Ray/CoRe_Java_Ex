fa4f09e053195e5397fabdf4e22e802b338aedde
CoDeX-Ray <raymundo3deborja@gmail.com>
2024-11-16 12:15:34 +0800

    Java Exercise #1

---
+++ src/jvEx1.java
@@ -0,0 +1,15 @@
+/*
+ Problem 1:
+    Write a Java program to print 'Hello' on screen and your
+    name on a separate line.
+
+Expected Output :
+    Hello
+    Alexandra Abramov
+*/
+
+public class jvEx1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello \nCodex Ray");
+    }
+}
