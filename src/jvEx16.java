60070269cebf59a99637a5e372c5c63ec3191d07
CoDeX-Ray <raymundo3deborja@gmail.com>
2024-11-17 08:29:18 +0800

    Java Exercise #16

---
+++ src/jvEx16.java
@@ -0,0 +1,18 @@
+/*
+ Problem 16: Write a Java program to print a face.
+ Expected Output
+  +"""""+
+ [| o o |]
+  |  ^  |
+  | '-' |
+  +-----+
+*/
+
+public class jvEx16 {
+    public static void main(String[] args) {
+        System.out.println("  +\"\"\"\"\"+");
+        System.out.println(" [| o o |]");
+        System.out.println("  |  ^  |");
+        System.out.println("  +-----+");
+    }
+}
